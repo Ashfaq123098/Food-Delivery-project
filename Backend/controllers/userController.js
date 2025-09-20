ab37a1488e7b68b35256a0a753fe828005027453
Ashfaq Ahsan <ashfaqbhai4560@gmail.com>
2025-09-20 13:35:43 +0600

    Add existing controllers folder and its files

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,91 @@
+
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+
+//login user
+const loginUser = async (req,res) =>{
+    const {email,password} = req.body;
+    try {
+        
+        const user = await userModel.findOne({email});
+
+        if (!user) {
+            return res.json({success:false,message:"User DoesNot Exist.Please Try Again"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if (!isMatch) {
+            return res.json({success:false,message:"Invalid PassWord"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error.Please Try Again"})
+    }
+}
+
+const createToken = (id) =>{
+    
+    return jwt.sign({id},process.env.JWT_SECRET)
+    }
+
+//register user
+const registerUser = async(req,res)=>{
+    const{name,password,email} = req.body;
+    try {
+        //Checking Is User Already Exists
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false,message:"User Already Found"})
+        }
+
+        //validating  valid email format and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false,message:"Please Enter A Valid Email"})
+        }
+         // 3. Check if local part is not only numbers
+        const localPart = email.split("@")[0];
+        if (/^\d+$/.test(localPart))
+            return res.json({ success: false, message: "Email cannot be only numbers" });
+        
+
+        const allowedDomains = ["gmail.com", "yahoo.com"];
+        const domain = email.split("@")[1];
+        if (!allowedDomains.includes(domain)) {
+            return res.json({ success: false, message: "Please Use a Valid Email Domain" });
+        }
+
+        if(password.length<8){
+            return res.json({success:false,message:"Please Enter A Strong Password"})
+        }
+
+        //hashing password
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+       const user =  await newUser.save()
+       const token = createToken(user._id)
+       res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error.Please Try Again"})
+    }
+}
+
+export {loginUser,registerUser}
