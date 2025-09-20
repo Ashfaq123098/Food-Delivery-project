bf61428fc0d6e7fe7a4d1cf2af042bf79439acd7
Ashfaq Ahsan <ashfaqbhai4560@gmail.com>
2025-09-20 17:59:25 +0600

    list item

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -7,7 +7,7 @@ const List = () => {
   const [localItems, setLocalItems] = useState([]);
   const [backendItems, setBackendItems] = useState([]);
 
-  // Fetch backend products
+  
   const fetchBackend = async () => {
     try {
       const res = await axios.get("http://localhost:4000/api/products");
